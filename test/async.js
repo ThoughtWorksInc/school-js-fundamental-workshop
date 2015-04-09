ee052f6c1eb508202af59107f9a2a083d3f84c1a
Eric Zeng <zengleizzz@gmail.com>
2015-04-09 16:05:36 +0800

    async test case

---
+++ test/async.js
@@ -0,0 +1,15 @@
+describe('Async Function', function() {
+  it('should have a callback to return its status or values', function(done) {
+    this.timeout(3000)
+    function delay(callback, arg) {
+      setTimeout(function() {
+        callback && callback(arg)
+      }, 2000)
+    }
+    function job(arg) {
+      arg.should.be.equal('finished')
+      done()
+    }
+    delay(job, 'finished')
+  })
+})
