22149de3efe6de06079e6e2416372a0384dfb613
Eric Zeng <zengleizzz@gmail.com>
2015-04-09 14:08:42 +0800

    closure base

---
+++ test/closure.spec.js
@@ -0,0 +1,19 @@
+describe('closure test cases', function() {
+  it('a closure should remember its scope', function() {
+    var bread = 'bread';
+    var fetchBread = function() {
+      return bread
+    }
+    var runingShop = function() {
+      var freshBread = 'freshBread';
+      return {
+        fetch: function() {
+          return freshBread
+        }
+      }
+    }
+    var shop = runingShop();
+    fetchBread().should.be.equal(bread);
+    shop.fetch().should.be.equal('freshBread')
+  })
+})
